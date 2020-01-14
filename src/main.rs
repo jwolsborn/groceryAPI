e02cd741bc60b6f77cd55706c71b210a3e72b665
Josh Wolsborn <wolsborn@pdx.edu>
2020-01-14 11:34:50 -0800

    Added status code to responses

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, http::StatusCode};
 use std::sync::Mutex;
 use serde::Serialize;
 
@@ -11,20 +11,21 @@ async fn add(req: HttpRequest, data: web::Data<Mutex<Groceries>>) -> impl Respon
     let mut data = data.lock().unwrap();
     let item = req.match_info().get("item").unwrap();
     data.items.push(item.to_string());
-    format!("Item {} added", &item)
+    web::Json(format!("Item {} added", &item)).with_status(StatusCode::OK)
 }
 
 async fn get(data: web::Data<Mutex<Groceries>>) -> impl Responder {
     let data = data.lock().unwrap();
-    web::Json(data.items.clone())
+    web::Json(data.items.clone()).with_status(StatusCode::OK)
 }
 
 async fn remove(req: HttpRequest, data: web::Data<Mutex<Groceries>>) -> impl Responder {
     let mut data = data.lock().unwrap();
     let item = req.match_info().get("item").unwrap();
     data.items.retain(|x| x != item);
-    format!("Item {} removed", &item) 
+    web::Json(format!("Item {} removed", &item)).with_status(StatusCode::OK) 
 }
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
