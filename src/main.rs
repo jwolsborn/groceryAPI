1fc33a0ad1b0885819bd51dc29d8060b8e8871eb
Josh <wolsborn@pdx.edu>
2020-01-15 17:02:15 -0800

    Removed unwraps.  Added JSON error handling

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,44 @@ struct Groceries {
     items: Vec<String>,
 }
 
+#[derive(Serialize)]
+struct JsonRes {
+	data: Vec<String>,
+	message: String,
+}
+
+
+
 async fn add(req: HttpRequest, data: web::Data<Mutex<Groceries>>) -> impl Responder {
-    let mut data = data.lock();
-    let item = req.match_info().get("item");
+    let mut data = match data.lock(){
+		Err(_) => return web::Json(format!("Data is locked")).with_status(StatusCode::BAD_REQUEST),
+		Ok(data) => data
+	};
+    let item = match req.match_info().get("item"){
+		None => return web::Json(format!("Unable to retrieve request")).with_status(StatusCode::BAD_REQUEST),
+		Some(item) => item
+	};
     data.items.push(item.to_string());
     web::Json(format!("Item {} added", &item)).with_status(StatusCode::OK)
 }
 
 async fn get(data: web::Data<Mutex<Groceries>>) -> impl Responder {
-    let data = data.lock();
-    web::Json(data.items.clone()).with_status(StatusCode::OK)
+    let data = match data.lock(){
+   		Err(_) => return web::Json(JsonRes{data: Vec::new(), message:"Data is locked".to_string()}).with_status(StatusCode::BAD_REQUEST),
+   		Ok(data) => data
+   };                                                                                        
+	web::Json(JsonRes{data:data.items.clone(), message: "Success".to_string()}).with_status(StatusCode::OK)
 }
 
 async fn remove(req: HttpRequest, data: web::Data<Mutex<Groceries>>) -> impl Responder {
-    let mut data = data.lock();
-    let item = req.match_info().get("item");
+    let mut data = match data.lock(){
+    	Err(_) => return web::Json(format!("Data is locked")).with_status(StatusCode::BAD_REQUEST),
+    	Ok(data) => data
+	};
+	let item = match req.match_info().get("item"){
+		None => return web::Json(format!("Unable to retrieve request")).with_status(StatusCode::BAD_REQUEST),
+		Some(item) => item
+	};
     data.items.retain(|x| x != item);
     web::Json(format!("Item {} removed", &item)).with_status(StatusCode::OK) 
 }
